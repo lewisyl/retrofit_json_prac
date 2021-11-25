5d987ea2691e4797ad1d5496d3bd918850c7b18c
Lewis Liu <lewisliuusa@outlook.com>
2021-11-25 08:27:44 -0800

    added loading circle feature

--- app/src/main/java/dev/lewisliu/retrofitjson/MainActivity.java
+++ app/src/main/java/dev/lewisliu/retrofitjson/MainActivity.java
@@ -6,7 +6,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Toast;
+
+import com.lusfold.spinnerloading.SpinnerLoading;
 
 import java.util.List;
 
@@ -17,33 +21,48 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private CustomAdapter adapter;
     private RecyclerView recyclerView;
-    ProgressDialog progressDoalog;
+    SpinnerLoading spinnerLoading;
+//    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDoalog = new ProgressDialog(MainActivity.this);
-        progressDoalog.setMessage("Loading....");
-        progressDoalog.show();
+//        progressDialog = new ProgressDialog(MainActivity.this);
+//        progressDialog.setMessage("Loading..");
+//        progressDialog.setTitle("Get Data");
+//        progressDialog.setIndeterminate(false);
+//        progressDialog.setCancelable(false);
+//        progressDialog.show();
 
-        /*Create handle for the RetrofitInstance interface*/
-        IGetDataService service = RetrofitClientInstance.getRetrofitInstance().create(IGetDataService.class);
-        Call<List<RetroPhoto>> call = service.getAllPhotos();
-        call.enqueue(new Callback<List<RetroPhoto>>() {
-            @Override
-            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
-                progressDoalog.dismiss();
-                generateDataList(response.body());
-            }
+        spinnerLoading = findViewById(R.id.spinner);
+        spinnerLoading.setPaintMode(1);
+        spinnerLoading.setCircleRadius(20);
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
-                progressDoalog.dismiss();
-                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            public void run() {
+                IGetDataService service = RetrofitClientInstance.getRetrofitInstance().create(IGetDataService.class);
+                Call<List<RetroPhoto>> call = service.getAllPhotos();
+                call.enqueue(new Callback<List<RetroPhoto>>() {
+                    @Override
+                    public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
+//                progressDialog.dismiss();
+                        generateDataList(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
+//                progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                spinnerLoading.setVisibility(View.GONE);
             }
-        });
+        }, 4000);
+
     }
     private void generateDataList(List<RetroPhoto> photoList) {
         recyclerView = findViewById(R.id.customRecyclerView);
